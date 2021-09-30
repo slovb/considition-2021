42fea01de62b32425cc80e8d1cb09d20f8380069
Rickard Nyman <4slovb@gmail.com>
2021-09-30 21:17:17 +0200

    textdumpar av körningar

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-from greedy_solver import GreedySolver
+from greedy_solver import GreedySolver as Solver
 import api
 import json
 
 with open('secret/apikey.txt', 'r') as f:
     api_key = f.read().rstrip('\n')
 
-map_name = "training1"
 
-
-def main():
-	print("Starting game...")
+def main(map_name):
 	response = api.new_game(api_key, map_name)
-	greedy = GreedySolver(game_info=response)
-	solution = greedy.Solve()
+	solver = Solver(game_info=response)
+	solution = solver.Solve()
 	submit_game_response = api.submit_game(api_key, map_name, solution)
-	print(submit_game_response)
+	log_solution(response, submit_game_response)
+
+
+def log_solution(response: dict, submit_game_response: dict) -> None:
+	map_name = response['mapName']
+	score = submit_game_response['score']
+	valid = submit_game_response['valid']
+	link = submit_game_response['link'].split(' ')[-1]
+
+	text = '{}, {}, {}, {}'.format(map_name, score, valid, link)
+	print(text)
+
+	if not valid:
+		return
+	filename = 'log/solution_{}.txt'.format(map_name)
+	log(filename, text)
+
+
+def log(filename: str, text: str) -> None:
+	from datetime import datetime
+	now = datetime.now().strftime("%Y%m%d, %H:%M:%S")
+	with open(filename, 'a') as f:
+		f.write('{}, {}\n'.format(now, text))
+
 
 if __name__ == "__main__":
-    main()
+	from sys import argv
+	if len(argv) < 2:
+		print('missing parameter')
+		exit()
+	main(argv[1])
