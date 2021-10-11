b4653b648c5b700232c17862edd8c89c4abf3729
Rickard Nyman <nyman.rickard@gmail.com>
2021-10-11 21:19:52 +0200

    7373

--- trainer_multiplier.py
+++ trainer_multiplier.py
@@ -27,23 +27,15 @@ def main() -> None:
         PENALTY_NOT_HEAVY=900,
         PENALTY_BOUNDING_BREAK=9000000,
         
-        MUL_X = 1.33, 
-        MUL_BOUNDING = 1.09,
-        MUL_WEIGHT = 10000.1,
-        MUL_SIDE_ALIGN = 1.09,
-        MUL_ORDER_SKIP = 0.81,
-        MUL_ORDER_BREAK = 10890.0,
+        # MUL_X = 1.33, MUL_BOUNDING = 1.09, MUL_WEIGHT = 10000.1, MUL_SIDE_ALIGN = 1.09, MUL_ORDER_SKIP = 0.81, MUL_ORDER_BREAK = 10890.0,
+        # MUL_X = 2.3561761300000015, MUL_BOUNDING = 1.579910310000001, MUL_WEIGHT = 1067200.2435293144, MUL_SIDE_ALIGN = 0.6436341000000001, MUL_ORDER_SKIP = 0.7290000000000001, MUL_ORDER_BREAK = 1056517.6759172622,
+        MUL_X = 3.1360704290300028, MUL_BOUNDING = 1.7205223275900012, MUL_WEIGHT = 3684264.6807295624, MUL_SIDE_ALIGN = 0.6436341000000001, MUL_ORDER_SKIP = 3.7219281189493496, MUL_ORDER_BREAK = 1035492.974166509
     )
     
+    step = 0.5
     searcher = Searcher(get_runner(['training1', 'training2']))
-    searcher.search(config, [
-        ('MUL_X', config.MUL_X, 0.1),
-        ('MUL_BOUNDING', config.MUL_BOUNDING, 0.1),
-        ('MUL_WEIGHT', config.MUL_WEIGHT, 0.1),
-        ('MUL_SIDE_ALIGN', config.MUL_SIDE_ALIGN, 0.1),
-        ('MUL_ORDER_SKIP', config.MUL_ORDER_SKIP, 0.1),
-        ('MUL_ORDER_BREAK', config.MUL_ORDER_BREAK, 0.1),
-    ], options_builder=scaling_options)
+    settings = [(name, config[name], step) for name in ['MUL_X', 'MUL_BOUNDING', 'MUL_WEIGHT', 'MUL_SIDE_ALIGN', 'MUL_ORDER_SKIP', 'MUL_ORDER_BREAK']]
+    searcher.search(config, settings, options_builder=scaling_options)
 
 
 if __name__ == "__main__":
