e7bfe6c98948e6ecaa41078c0328620df11d0d60
Rickard Nyman <nyman.rickard@gmail.com>
2021-10-13 22:24:36 +0200

    uppdaterad main

--- main.py
+++ main.py
@@ -11,10 +11,56 @@ fetcher = lambda id: api.fetch_game(api_key, id)['gameStats']
 
 
 def main(maps: list[str]) -> None:
+    config = Config(
+        LOG_RESIZE=False,
+        LOG_PLACED=False,
+        ENABLE_LIMIT_NUM_CANDIDATES=False,
+        PREFERRED_NUM_CANDIDATES=150,
+        
+        ENABLE_OPTIMAL_DISTANCE=False,
+        MUL_OPTIMAL_X=1.0,
+        MUL_OPTIMAL_Y=1.0,
+        MUL_OPTIMAL_Z=1.0,
+        
+        ENABLE_HEAVY_PRIORITY=True,
+        PENALTY_NOT_HEAVY=42.15992028364802,
+        
+        ENABLE_WEIGHT=True,
+        PENALTY_HEAVY_ON_LIGHT=45.927,
+        PENALTY_HEAVY_ON_MEDIUM=11.1537,
+        PENALTY_HEAVY_ON_HEAVY=0,
+        MUL_WEIGHT=27036.439482589834,
+        
+        ENABLE_SIDE_ALIGN=True,
+        MUL_SIDE_ALIGN=1.1261022213600007,
+        
+        ENABLE_X=True,
+        MUL_X=6.985596880664332,
+        EXP_X=1.9800000000000002,
+        
+        ENABLE_BOUNDING=True,
+        PENALTY_BOUNDING_BREAK=88329.8219153818,
+        MUL_BOUNDING=1.9355876185387513,
+        EXP_BOUNDING=1.0,
+        
+        ENABLE_ORDER_SKIP=True,
+        ORDER_BASE_REDUCTION=1000,
+        ORDER_BASE=20.287098,
+        EXP_ORDER_N=2,
+        MUL_ORDER_SKIP=1.6279713592284457,
+        EXP_ORDER_SKIP=2.4200000000000004,
+        
+        ENABLE_ORDER_BREAK=True,
+        MUL_ORDER_BREAK=8.143448106597244e3,
+        
+        ENABLE_BOUNDED_X=True,
+        MUL_BOUNDED_X=178120.88369971208,
+    )
+    
     games = [Game(api.new_game(api_key, map)) for map in maps]
    
     for game in games:
-        placed_packages = game.solve(None)
+        placed_packages = game.solve(config)
         solution = [pp.as_solution() for pp in placed_packages]
         submit_game_response = api.submit_game(api_key, game.mapName(), solution)
         if submit_game_response is not None:
--- trainer_simple.py
+++ trainer_simple.py
@@ -8,44 +8,57 @@ def main() -> None:
         LOG_RESIZE=False,
         LOG_PLACED=False,
         ENABLE_LIMIT_NUM_CANDIDATES=False,
+        PREFERRED_NUM_CANDIDATES=150,
+        
+        ENABLE_OPTIMAL_DISTANCE=False,
+        MUL_OPTIMAL_X=1.0,
+        MUL_OPTIMAL_Y=1.0,
+        MUL_OPTIMAL_Z=1.0,
         
         ENABLE_HEAVY_PRIORITY=True,
-        PENALTY_NOT_HEAVY=79.33132800000003,
+        PENALTY_NOT_HEAVY=42.15992028364802,
+        
         ENABLE_WEIGHT=True,
-        PENALTY_HEAVY_ON_LIGHT=70,
-        PENALTY_HEAVY_ON_MEDIUM=17,
+        PENALTY_HEAVY_ON_LIGHT=45.927,
+        PENALTY_HEAVY_ON_MEDIUM=11.1537,
         PENALTY_HEAVY_ON_HEAVY=0,
         MUL_WEIGHT=27036.439482589834,
+        
         ENABLE_SIDE_ALIGN=True,
         MUL_SIDE_ALIGN=1.1261022213600007,
+        
         ENABLE_X=True,
-        MUL_X=7.056158465317506,
-        EXP_X=2,
+        MUL_X=6.985596880664332,
+        EXP_X=1.9800000000000002,
+        
         ENABLE_BOUNDING=True,
         PENALTY_BOUNDING_BREAK=88329.8219153818,
         MUL_BOUNDING=1.9355876185387513,
         EXP_BOUNDING=1.0,
+        
         ENABLE_ORDER_SKIP=True,
+        ORDER_BASE_REDUCTION=1000,
         ORDER_BASE=20.287098,
         EXP_ORDER_N=2,
         MUL_ORDER_SKIP=1.6279713592284457,
-        EXP_ORDER_SKIP=2,
+        EXP_ORDER_SKIP=2.4200000000000004,
         
-        ENABLE_ORDER_BREAK=False,
-        MUL_ORDER_BREAK=8.143448106597244e-12,
+        ENABLE_ORDER_BREAK=True,
+        MUL_ORDER_BREAK=8.143448106597244e3,
         
         ENABLE_BOUNDED_X=True,
         MUL_BOUNDED_X=178120.88369971208,
     )
     
     maps = ['training1', 'training2']
-    # runner = get_randomized_runner(maps)
     runner = get_runner(maps)
     score = runner(config)
     print('baseline: {}'.format(score))
     print('----------------------------------------------------------------')
     for step in [0.2] * 5:
         for attr in [
+            'MUL_ORDER_BREAK',
+            'ORDER_BASE_REDUCTION',
             'MUL_BOUNDED_X',
             'PENALTY_BOUNDING_BREAK',
             'ORDER_BASE',
@@ -70,8 +83,6 @@ def main() -> None:
                 print('no improvement from {} to {}'.format(getattr(config, attr), cop.name))
             config = cop.action(config) # just in case the attribute got decreased
         print(config)
-        # print('randomize')
-        # runner = get_randomized_runner(maps)
 
 
 if __name__ == "__main__":
