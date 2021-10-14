0cdc1a0ec49fc360d3b0c0b032f458b94641a3c8
Rickard Nyman <nyman.rickard@gmail.com>
2021-10-14 07:40:43 +0200

    bättre config

--- trainer_multiple.py
+++ trainer_multiple.py
@@ -1,9 +1,13 @@
+import itertools
+import random
+
 from runner import get_runner, get_randomized_runner
 from solver.config import Config
 from simple_searcher import SimpleSearcher
 
 
-def main() -> None:  
+def main() -> None:
+    '''
     config = Config(
         LOG_RESIZE=False,
         LOG_PLACED=False,
@@ -49,68 +53,31 @@ def main() -> None:
         ENABLE_BOUNDED_X=True,
         MUL_BOUNDED_X=178120.88369971208,
     )
+    '''
+    config = Config(RANDOMIZE=False, LOG_RESIZE=False, LOG_PLACED=False, ENABLE_LIMIT_NUM_CANDIDATES=False, PREFERRED_NUM_CANDIDATES=150, ENABLE_OPTIMAL_DISTANCE=False, MUL_OPTIMAL_X=1.0, MUL_OPTIMAL_Y=1.0, MUL_OPTIMAL_Z=1.0, ENABLE_HEAVY_PRIORITY=True, PENALTY_NOT_HEAVY=42.15992028364802, ENABLE_WEIGHT=True, PENALTY_HEAVY_ON_LIGHT=45.927, PENALTY_HEAVY_ON_MEDIUM=11.1537, PENALTY_HEAVY_ON_HEAVY=0, MUL_WEIGHT=91247.98325374069, ENABLE_SIDE_ALIGN=True, MUL_SIDE_ALIGN=1.1261022213600007, ENABLE_X=True, MUL_X=6.985596880664332, EXP_X=1.9800000000000002, ENABLE_BOUNDING=True, PENALTY_BOUNDING_BREAK=88329.8219153818, MUL_BOUNDING=1.9355876185387513, EXP_BOUNDING=1.0, ENABLE_ORDER_SKIP=True, ORDER_BASE_REDUCTION=65.53600000000002, ORDER_BASE=20.287098, EXP_ORDER_N=2, MUL_ORDER_SKIP=4.861096431114389, EXP_ORDER_SKIP=2.4200000000000004, ENABLE_ORDER_BREAK=True, MUL_ORDER_BREAK=4886.068863958346, ENABLE_BOUNDED_X=True, MUL_BOUNDED_X=178120.88369971208)
     
     maps = ['training1', 'training2']
+    searchspace = list(itertools.combinations([
+        'MUL_ORDER_SKIP',
+        'MUL_ORDER_BREAK',
+        'ORDER_BASE_REDUCTION',
+        'MUL_WEIGHT',
+        'EXP_ORDER_SKIP',
+        'ORDER_BASE',
+        'EXP_ORDER_N',
+        'MUL_BOUNDED_X',
+        'MUL_BOUNDING',
+        'EXP_BOUNDING',
+        'MUL_SIDE_ALIGN',
+        'PENALTY_NOT_HEAVY',
+    ], 2))
     # runner = get_randomized_runner(maps)
     runner = get_runner(maps)
     score = runner(config)
     print('baseline: {}'.format(score))
     print('----------------------------------------------------------------')
-    for step in [0.5, 0.1, 0.05]:
-        for attrs in [
-            [
-                'MUL_ORDER_BREAK',
-            ],
-            [   
-                'ORDER_BASE_REDUCTION',
-            ],
-            [
-                'MUL_WEIGHT',
-            ],
-            [
-                'ORDER_BASE',
-                'EXP_ORDER_SKIP',
-            ],
-            [
-                'MUL_ORDER_SKIP', 
-            ],
-            [
-                'MUL_ORDER_BREAK',
-                'ORDER_BASE_REDUCTION',
-                'ORDER_BASE',
-                'EXP_ORDER_SKIP',
-                'MUL_ORDER_SKIP',
-            ]]:
-            '''
-            [
-                'MUL_WEIGHT',
-                'MUL_SIDE_ALIGN',
-                'MUL_ORDER_SKIP',
-            ],
-            [
-                'MUL_BOUNDED_X',
-                'PENALTY_BOUNDING_BREAK',
-            ],
-            [
-                'ORDER_BASE',
-                'EXP_ORDER_SKIP',
-            ],
-            [
-                'EXP_X',
-                'MUL_X',
-            ],
-            [
-                'EXP_BOUNDING',
-                'MUL_BOUNDING',
-            ],
-            [
-                'PENALTY_HEAVY_ON_LIGHT',
-                'PENALTY_HEAVY_ON_MEDIUM',  
-            ],
-            [
-                'PENALTY_NOT_HEAVY',
-            ],
-            '''
+    for step in [0.2, 0.1, 0.05]:
+        for attrs in random.sample(searchspace, len(searchspace)):
             searcher = SimpleSearcher(runner)
             settings = [(name, getattr(config, name), step) for name in attrs]
             cop, s = searcher.search(config, settings)
@@ -119,6 +86,7 @@ def main() -> None:
                 print('best ({}) from {}'.format(score, cop.name))
             else:
                 print('no improvement from {}'.format(cop.name))
+            print('===============================')
             config = cop.action(config) # just in case the attribute got decreased
         print(config)
         # print('randomize')
